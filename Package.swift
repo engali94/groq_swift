afca1623381b9c7576cae0e08d2afe7c1f7c89d2
Ali A. Hilal <ali94_200970@yahoo.com>
2025-02-24 23:45:13 +0300

    Simplify Package.swift configuration and remove experimental settings

--- Package.swift
+++ Package.swift
@@ -9,28 +9,23 @@ let package = Package(
         .iOS(.v16),
         .watchOS(.v9),
         .tvOS(.v16),
-        .visionOS(.v1),
+        .visionOS(.v1)
     ],
     products: [
         .library(
             name: "GroqSwift",
-            targets: ["GroqSwift"]),
+            targets: ["GroqSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "GroqSwift",
-            dependencies: [],
-            swiftSettings: [
-                .enableExperimentalFeature("StripedBuildSettings")
-            ],
-            plugins: [
-                .plugin(name: "Swift-DocC", package: "swift-docc-plugin")
-            ]),
+            dependencies: []
+        ),
         .testTarget(
             name: "GroqSwiftTests",
-            dependencies: ["GroqSwift"]),
+            dependencies: ["GroqSwift"])
     ]
 )
