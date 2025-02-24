a33b8adf6ce7c3b0a797c51fc6caa5cd17ef714a
Ali A. Hilal <ali94_200970@yahoo.com>
2025-02-24 23:44:35 +0300

    Update Package.swift and CI workflows to use Xcode 16.2

--- Package.swift
+++ Package.swift
@@ -9,23 +9,28 @@ let package = Package(
         .iOS(.v16),
         .watchOS(.v9),
         .tvOS(.v16),
-        .visionOS(.v1)
+        .visionOS(.v1),
     ],
     products: [
         .library(
             name: "GroqSwift",
-            targets: ["GroqSwift"])
+            targets: ["GroqSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "GroqSwift",
-            dependencies: []
-        ),
+            dependencies: [],
+            swiftSettings: [
+                .enableExperimentalFeature("StripedBuildSettings")
+            ],
+            plugins: [
+                .plugin(name: "Swift-DocC", package: "swift-docc-plugin")
+            ]),
         .testTarget(
             name: "GroqSwiftTests",
-            dependencies: ["GroqSwift"])
+            dependencies: ["GroqSwift"]),
     ]
 )
